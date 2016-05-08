a411258401e3707f3ad43a4ea96f9ccc3a214848
Simone Genovese <genovese@esteco.com>
2016-05-08 22:32:45 +0200

    FirstCommit

---
+++ BBRemote.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Simone Genovese on 08/05/16.
+//
+
+#include "blynk-library/linux/BlynkApiWiringPi.h"
+#include "blynk-library/linux/BlynkSocket.h"
+#include "blynk-library/linux/BlynkOptionsParser.h"
+
+static BlynkTransportSocket _blynkTransport;
+BlynkSocket Blynk(_blynkTransport);
+
+#include "blynk-library/BlynkWidgets.h"
+
+BLYNK_WRITE(V1) {
+        printf("Got a value: %s\n", param[0].asStr());
+}
+
+BLYNK_WRITE(V2) {
+        printf("Reboot: %s\n", param[0].asStr());
+        system("init 6");
+}
+
+int main(int argc, char *argv[]) {
+    const char *auth, *serv, *port;
+    parse_options(argc, argv, auth, serv, port);
+
+    Blynk.begin(auth, serv, port);
+
+    while (true) {
+        Blynk.run();
+    }
+}
+
